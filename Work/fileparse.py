485a48a48cc63f775e1ba728bdf04f28e298570d
Auntiewhnor Kpolie <59895366+AuntiewhnorK@users.noreply.github.com>
2022-05-06 22:02:02 -0400

    parsing csv files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,43 @@
-# fileparse.py
-#
-# Exercise 3.3
+"""
+Practical Python - 3.3 Making general purpose functions
+
+Auntiewhnor Kpolie
+Created: 05/06/2022
+Last Edited:
+
+"""
+import csv
+
+from numpy import indices
+from requests import head
+
+
+def parse_csv(filename, select=None):
+    """
+    Parse CSV file into a list of records
+    """
+    with open(filename) as f:
+        rows = csv.reader(f)
+
+        headers = next(rows)
+
+        # if select columns are chosen, find those indices
+        if select:
+            indices = [headers.index(column) for column in select]
+            headers = select
+        else:
+            indices = []
+
+        records = []
+        for row in rows:
+            if not row:  # Skip rows with no data
+                continue
+            # filter row on selected columns
+            if indices:
+                row = [row[index] for index in indices]
+
+            # create dictionary record
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
