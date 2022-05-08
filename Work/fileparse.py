ea440e2bc5645b6ed162dc3562df314f5d269010
Auntiewhnor Kpolie <59895366+AuntiewhnorK@users.noreply.github.com>
2022-05-07 22:36:54 -0400

    update parse_csv function

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@ Practical Python - 3.3 Making general purpose functions
 
 Auntiewhnor Kpolie
 Created: 05/06/2022
-Last Edited:
+Last Edited: 05/07/2022
 
 """
 import csv
@@ -12,14 +12,22 @@ from numpy import indices
 from requests import head
 
 
-def parse_csv(filename, select=None):
+def parse_csv(filename, select=None, types=None, has_headers=True,
+              delimit=',', silence_errors=True):
     """
-    Parse CSV file into a list of records
+    Parse CSV file into a list of records.
     """
     with open(filename) as f:
-        rows = csv.reader(f)
+        rows = csv.reader(f, delimiter=delimit)
+        # Raise error if select and has_headers=False is passed
 
-        headers = next(rows)
+        if select and not has_headers:
+            raise RuntimeError('select argument needs column headers')
+
+        if has_headers:
+            headers = next(rows)
+        else:
+            headers = []
 
         # if select columns are chosen, find those indices
         if select:
@@ -29,15 +37,30 @@ def parse_csv(filename, select=None):
             indices = []
 
         records = []
-        for row in rows:
+        for rownum, row in enumerate(rows, start=1):
             if not row:  # Skip rows with no data
                 continue
             # filter row on selected columns
             if indices:
                 row = [row[index] for index in indices]
+            # preform type conversion (ex: int('100') -> 100)
+            try:
+                if types:
+                    row = [func(val) for func, val in zip(types, row)]
+            except ValueError as v:
+                if silence_errors:
+                    continue
+                else:
+                    print(f"Row {rownum}: Couldn't parse {row}")
+                    print(f"Reason: {v}")
+                    continue
 
-            # create dictionary record
-            record = dict(zip(headers, row))
-            records.append(record)
+            if has_headers and headers != "":
+                # create dictionary record
+                record = dict(zip(headers, row))
+                records.append(record)
+            else:
+                record = tuple(row)
+                records.append(record)
 
     return records
