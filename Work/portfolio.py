1259c3b8ab19fa49a3f34c1a6e94cc5f3fbdb64b
Auntiewhnor Kpolie <59895366+AuntiewhnorK@users.noreply.github.com>
2022-06-07 19:58:43 -0400

    add 3 new methods

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -1,5 +1,6 @@
 class Portfolio:
     """
+    Container class 
     Layer around a portfolio list
     """
 
@@ -10,9 +11,19 @@ class Portfolio:
     def __iter__(self):
         return self._holdings.__iter__()
 
+    # other methods
+    def __len__(self):
+        return len(self._holdings)
+
+    def __getitem__(self, index):
+        return self._holdings[index]
+
+    def __contains__(self, name):
+        return any([s.name == name for s in self._holdings])
+
     @property
     def total_cost(self):
-        return sum([s.cost for s in self._holdings])
+        return sum([s.shares*s.price for s in self._holdings])
 
     def tabulate_shares(self):
         from collections import Counter
