5e7d9fe6454362f6254b69dceb08dbcd0da67f3a
Auntiewhnor Kpolie <59895366+AuntiewhnorK@users.noreply.github.com>
2022-06-18 17:12:51 -0400

    add ticker function and format option

--- Work/ticker.py
+++ Work/ticker.py
@@ -6,7 +6,8 @@ Created: 6/17/2022
 
 Reads in portfolio csv file and outputs rows
 """
-
+import report2
+import tableformat
 from follow import follow
 import csv
 
@@ -36,29 +37,49 @@ def make_dicts(rows, headers):
         yield dict(zip(headers, row))
 
 
+def parse_stock_data(lines):
+    """
+    Read in stock data
+    """
+    row = csv.reader(lines)
+    rows = select_columns(row, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ["name", "price", "change"])
+    return rows
+
+
 def filter_symbols(rows, names):
     """
     Filter rows based on user argument
     """
     for row in rows:
-        if row['name'] in names:
+        if row["name"] in names:
             yield row
 
 
-def parse_stock_data(lines):
-    row = csv.reader(lines)
-    rows = select_columns(row, [0, 1, 4])
-    rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, ['name', 'price', 'change'])
-    return rows
-
-
-if __name__ == '__main__':
-    # filter stocks in Data/portfolio.csv
-
-    import report2
-    portfolio = report2.read_portfolio('Data/portfolio.csv')
-    rows = parse_stock_data(follow('Data/stocklog.csv'))
+def ticker(portfile, logfile, fmt):
+    """
+    Given portfolio and log file,
+    output data based on given format
+    """
+    portfolio = report2.read_portfolio(portfile)
+    lines = follow(logfile)
+    rows = parse_stock_data(lines)
     rows = filter_symbols(rows, portfolio)
+    formatter = tableformat.create_formatter(fmt)
+    formatter.headings(["Name", "Price", "Change"])
     for row in rows:
-        print(row)
+        formatter.row([row["name"], f"{row['price']:0.2f}", f"{row['change']:0.2f}"])
+
+
+def main(argument):
+    if len(argument) != 4:
+        raise SystemExit("Usage: %s portfoliofile logfile fmt" % argument[0])
+    ticker(argument[1], argument[2], argument[3])
+
+
+if __name__ == "__main__":
+
+    import sys
+
+    main(sys.argv)
