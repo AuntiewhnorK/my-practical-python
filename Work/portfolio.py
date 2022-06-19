8dde21478366e685e56c286034d3906c5b86d5b0
Auntiewhnor Kpolie <59895366+AuntiewhnorK@users.noreply.github.com>
2022-06-19 19:26:31 -0400

    replace list comprehension with generator

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -20,14 +20,15 @@ class Portfolio:
         return self._holdings[index]
 
     def __contains__(self, name):  # check 'item' in portfolio
-        return any([s.name == name for s in self._holdings])
+        return any(s.name == name for s in self._holdings)
 
     @property
     def total_cost(self):
-        return sum([s.shares*s.price for s in self._holdings])
+        return sum(s.shares * s.price for s in self._holdings)
 
     def tabulate_shares(self):
         from collections import Counter
+
         total_shares = Counter()
         for s in self._holdings:
             total_shares[s.name] += s.shares
